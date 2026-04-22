4c1d2229f4d2eaaa6f1bfcc1cd8258492b415953
Shina Ojo <ojoshinajohnson@gmail.com>
2026-04-22 04:18:21 +0100

    Fix comment formatting in addition.py

--- src/addition.py
+++ src/addition.py
@@ -3,7 +3,7 @@
 def add(a, b):
     return a + b
     
-#Test the addition function using the values specified
+#Test the addition function using the values
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
