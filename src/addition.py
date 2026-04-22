ace8fe47f8d76ca4c6018ef1f94b1bc4128433b7
Shina Ojo <ojoshinajohnson@gmail.com>
2026-04-22 04:33:57 +0100

    Clarify test comment for addition function
    Updated comment to clarify the purpose of the test.

--- src/addition.py
+++ src/addition.py
@@ -3,7 +3,7 @@
 def add(a, b):
     return a + b
     
-#Test the addition function using the values
+#Test the addition function using the values specified
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
